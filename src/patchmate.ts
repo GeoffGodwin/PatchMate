1f1e31c79dc447c8044a72e74f316cba36d82964
Geoff Godwin <geoff.godwin@gmail.com>
2025-03-10 11:11:31 -0400

    Updated dist to include entire folder.

--- src/patchmate.ts
+++ src/patchmate.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env node
 import { scanDependencies } from "./scanner.js";
 import { analyzeChangelog } from "./ai.js";
 import { createGitHubIssue, createGitHubPR } from "./github.js";
